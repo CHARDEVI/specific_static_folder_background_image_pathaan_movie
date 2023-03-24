53935ce57f6e4ddee608ba454ad11d2f2e62447e
CHARDEVI <deviprasadisaka@gmail.com>
2023-03-25 02:00:06 +0530

    first commit

---
+++ charishma/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CharishmaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'charishma'
---
+++ charishma/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def cherry(request):
+    return render(request,'cherry.html')
